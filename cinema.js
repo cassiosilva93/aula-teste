1ef9719ee0ec27b1f6d02a379a0539edcaec8870
Cassio Oliveira Silva <cos@vortx.com.br>
2022-03-10 10:24:55 -0300

    meu primeiro commit

---
+++ cinema.js
@@ -0,0 +1,42 @@
+const catalogo = require('./database/catalogo.json')
+
+function listarTodosOsFilmes() {
+  for(let i = 0; i < catalogo.length; i++) {
+    console.log(catalogo[i]);
+  }
+}
+
+function listarFilmesEmCartaz() {
+  let filmesEmCartaz = []
+
+  for(let i = 0; i < catalogo.length; i++) {
+    if (catalogo[i].emCartaz) {
+      filmesEmCartaz.push(catalogo[i])
+    }
+  }
+
+  return filmesEmCartaz
+}
+
+function adicionarFilme(filme) {
+  catalogo.push(filme)
+}
+
+function buscarFilme(codigo) { 
+  let filmeEncontrado = []
+
+  for(let i = 0; i < catalogo.length; i++) { 
+    if (catalogo[i].codigo === codigo) {
+      filmeEncontrado = catalogo[i]
+    }
+  }
+
+  return filmeEncontrado
+}
+
+function alterarEmCartaz(codigo, buscarFilme) { 
+  const filmeEncontrado = buscarFilme(codigo)
+  filmeEncontrado.emCartaz = !filmeEncontrado.emCartaz
+  return filmeEncontrado
+}
+
